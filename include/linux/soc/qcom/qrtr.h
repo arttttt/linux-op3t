f674178a01a41512e2b09dc7d889e8702e644b71
Yassine Oudjana <y.oudjana@protonmail.com>
2025-07-10 09:25:46 +0100

    net: qrtr: Define macro to convert QMI version and instance to QRTR instance
    Move QRTR instance conversion from qmi_interface into a new macro in order
    to reuse it in QRTR device ID tables.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>
    (cherry picked from commit 21d9533a4b527b536874f215e66c5e2057eb2aac)

--- include/linux/soc/qcom/qrtr.h
+++ include/linux/soc/qcom/qrtr.h
@@ -3,6 +3,8 @@
 #ifndef __QCOM_QRTR_H__
 #define __QCOM_QRTR_H__
 
+#include <linux/bitfield.h>
+#include <linux/bits.h>
 #include <linux/mod_devicetable.h>
 
 struct qrtr_device {
@@ -14,6 +16,14 @@ struct qrtr_device {
 };
 
 #define to_qrtr_device(d) container_of(d, struct qrtr_device, dev)
+
+#define QRTR_INSTANCE(qmi_version, qmi_instance) \
+	(FIELD_PREP(GENMASK(7, 0), qmi_version) | \
+	 FIELD_PREP(GENMASK(15, 8), qmi_instance))
+
+#define QRTR_INSTANCE_CONST(qmi_version, qmi_instance) \
+	(FIELD_PREP_CONST(GENMASK(7, 0), qmi_version) | \
+	 FIELD_PREP_CONST(GENMASK(15, 8), qmi_instance))
 
 struct qrtr_driver {
 	int (*probe)(struct qrtr_device *qdev);
